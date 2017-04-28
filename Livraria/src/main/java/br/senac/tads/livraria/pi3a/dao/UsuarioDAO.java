a2843de4e341ac127a71a6fc39774c78d11df647
Fernanda <Fernanda@FAM>
2017-04-28 18:11:17 -0300

    Inicio da Class UsuarioDAO.

---
+++ Livraria/src/main/java/br/senac/tads/livraria/pi3a/dao/UsuarioDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.livraria.pi3a.dao;
+
+import br.senac.tads.livraria.pi3a.model.Usuario;
+
+/**
+ *
+ * @author Fernanda
+ */
+public interface UsuarioDAO {
+    public void inserir(Usuario usuario);
+    
+}
