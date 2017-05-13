8f64be0ae651eb4d98e8fbb3656e162c88a31e06
Isaias <Isaias@isaias.senacsp.edu.br>
2017-05-13 14:04:14 -0300

    fazendo get e set para os campos

--- Livraria/src/main/java/br/senac/tads/livraria/pi3a/model/Usuario.java
+++ Livraria/src/main/java/br/senac/tads/livraria/pi3a/model/Usuario.java
@@ -5,30 +5,55 @@
  */
 package br.senac.tads.livraria.pi3a.model;
 
+import java.util.Date;
+
 /**
  *
  * @author Fernanda
  */
-public class Usuario{
+public class Usuario {
+
     private int id;
     private String nome;
     private String email;
     private String fixo;
-    
-    public Usuario(){
-        
+    private String cel;
+    private String setor;
+    private String sexo;
+    private String senha;
+    private String tipoAcesso;
+    private Date dataNasc;
+
+    public Usuario() {
+
     }
-    public Usuario(String nome, String email, String fixo) {
-    this.nome = nome;
-    this.email = email;
-    this.fixo = fixo;
-  }
-    public Usuario(int id, String nome,String email, String fixo) {
-    this.id = id;
-    this.nome = nome;
-    this.email = email;
-    this.fixo = fixo;
-  }
+
+    public Usuario(String nome, String email, String fixo, String cel, String setor,
+            String sexo, String senha, String tipoAcesso, Date dataNasc) {
+        this.nome = nome;
+        this.email = email;
+        this.fixo = fixo;
+        this.cel = cel;
+        this.setor = setor;
+        this.sexo = sexo;
+        this.senha = senha;
+        this.tipoAcesso = tipoAcesso;
+        this.dataNasc = dataNasc;
+    }
+
+    public Usuario(int id, String nome, String email, String fixo, String cel,
+            String setor, String sexo, String senha, String tipoAcesso, Date dataNasc) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.fixo = fixo;
+        this.cel = cel;
+        this.setor = setor;
+        this.sexo = sexo;
+        this.senha = senha;
+        this.tipoAcesso = tipoAcesso;
+        this.dataNasc = dataNasc;
+    }
 
     public int getId() {
         return id;
@@ -60,5 +85,53 @@ public class Usuario{
 
     public void setFixo(String fixo) {
         this.fixo = fixo;
+    }
+
+    public String getCel() {
+        return cel;
+    }
+
+    public void setCel(String cel) {
+        this.cel = cel;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexor) {
+        this.sexo = sexo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipoAcesso() {
+        return tipoAcesso;
+    }
+
+    public void setTipoAcesso(String tipoAcesso) {
+        this.tipoAcesso = tipoAcesso;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
     }
 }
