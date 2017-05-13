a65c3d1f02815719a366d52bedd4b141a878a706
Antonio <Antonio@Antonio>
2017-05-12 21:34:34 -0300

    Servlet da empresa iniciado

---
+++ Livraria/src/main/java/br/senac/tads/livraria/pi3a/controller/EmpresaServlet.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.livraria.pi3a.controller;
+
+import br.senac.tads.livraria.pi3a.dao.EmpresaDao;
+import br.senac.tads.livraria.pi3a.model.Empresa;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Douglas
+ */
+@WebServlet(name = "EmpresaServlet", urlPatterns = {"/EmpresaServlet"})
+public class EmpresaServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Usuario.jsp");
+        RequestDispatcher dispatcher
+                = request.getRequestDispatcher("empresa.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String cnpj = request.getParameter("cnpj");
+        String razao = request.getParameter("razao");
+
+        String ie = request.getParameter("inscricaoEstadual");
+        String telefone = request.getParameter("telefone");
+        String endereco = request.getParameter("endereco");
+        String numero = request.getParameter("numero");
+        String complemento = request.getParameter("complemento");
+        String cep = request.getParameter("cep");
+        String bairro = request.getParameter("bairro");
+        String cidade = request.getParameter("cidade");
+        String estado = request.getParameter("estado");
+
+        // Cria um novo contato e salva
+        // através do DAO
+        Empresa novo = new Empresa(cnpj,razao,ie,telefone,endereco,numero,complemento,cep,bairro,cidade,estado);
+        EmpresaDao dao = new EmpresaDao();
+        dao.incluirComTransacao(novo);
+
+        // Usa a sessao para manter os dados após
+        // redirect (técnica POST-REDIRECT-GET),
+        // usado para evitar dupla submissão dos
+        // dados
+        HttpSession sessao = request.getSession();
+        sessao.setAttribute("novaEmpresa", novo);
+        response.sendRedirect("resultado.jsp");
+
+    }
+}
