f96a3b37204e0d1e23e6f46b079893079aedfab2
Isaias <Isaias@isaias.senacsp.edu.br>
2017-05-22 19:45:28 -0300

    Criando select de busca

--- Livraria/src/main/java/br/senac/tads/livraria/pi3a/controller/UsuarioServlet.java
+++ Livraria/src/main/java/br/senac/tads/livraria/pi3a/controller/UsuarioServlet.java
@@ -3,11 +3,10 @@ package br.senac.tads.livraria.pi3a.controller;
 import br.senac.tads.livraria.pi3a.dao.UsuarioDAO;
 import br.senac.tads.livraria.pi3a.model.Usuario;
 import java.io.IOException;
-import java.text.ParseException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,9 +17,9 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Fernanda
+ * @author Isaias
  */
-@WebServlet(name = "UsuarioServlet", urlPatterns = {"/UsuarioServlet"})
+@WebServlet(name = "UsuarioServlet", urlPatterns = {"/teste"})
 public class UsuarioServlet extends HttpServlet {
 
     @Override
@@ -28,7 +27,7 @@ public class UsuarioServlet extends HttpServlet {
             throws ServletException, IOException {
         //RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Usuario.jsp");
         RequestDispatcher dispatcher
-                = request.getRequestDispatcher("bootstrap/usuario.jsp");
+                = request.getRequestDispatcher("usuario.jsp");
         dispatcher.forward(request, response);
 
     }
@@ -36,7 +35,6 @@ public class UsuarioServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         String nome = request.getParameter("nome");
 
         String sexo = request.getParameter("sexo");
@@ -47,27 +45,39 @@ public class UsuarioServlet extends HttpServlet {
         String senha = request.getParameter("senha");
         String tipoAcesso = request.getParameter("tipoAcesso");
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date dataNasc;
-
-        try {
-            dataNasc = sdf.parse(request.getParameter("dataNasc"));
-            Usuario novo = new Usuario(nome, email, fixo, cel, setor, sexo, senha, tipoAcesso, dataNasc);
-            UsuarioDAO dao = new UsuarioDAO();
-            dao.incluirComTransacao(novo);
-
-            HttpSession sessao = request.getSession();
-            sessao.setAttribute("novoUsuario", novo);
-            response.sendRedirect("resultado.jsp");
-
-        } catch (ParseException ex) {
-            Logger.getLogger(UsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Date dataNasc = null;
 
         // Cria um novo contato e salva
         // através do DAO
+        Usuario novo = new Usuario(nome, email, fixo, cel, setor, sexo, senha, tipoAcesso, dataNasc);
+        UsuarioDAO dao = new UsuarioDAO();
+        dao.incluirComTransacao(novo);
+
         // Usa a sessao para manter os dados após
         // redirect (técnica POST-REDIRECT-GET),
         // usado para evitar dupla submissão dos
         // dados
+        HttpSession sessao = request.getSession();
+        sessao.setAttribute("novoUsuario", novo);
+        response.sendRedirect("bootstrap/resultadoUsuario.jsp");
+
     }
 }
+
+//        String nome = request.getParameter("nome");
+//
+//        String sexo = request.getParameter("sexo");
+//        String email = request.getParameter("email");
+//        String fixo = request.getParameter("telefone");
+//        String cel = request.getParameter("celular");
+//        String setor = request.getParameter("setor");
+//        String senha = request.getParameter("senha");
+//        String tipoAcesso = request.getParameter("tipoAcesso");
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//        Date dataNasc;
+//
+//        try {
+//            dataNasc = sdf.parse(request.getParameter("dataNasc"));
+//            Usuario novo = new Usuario(nome, email, fixo, cel, setor, sexo, senha, tipoAcesso, dataNasc);
+//            UsuarioDAO dao = new UsuarioDAO();
+//            dao.incluirComTransacao(novo);
