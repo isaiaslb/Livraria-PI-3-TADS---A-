e6d916c12823643fd5dd8620eda3db75586f5230
Isaias <Isaias@isaias.senacsp.edu.br>
2017-05-22 19:45:07 -0300

    Atualizando classe

--- Livraria/src/main/java/br/senac/tads/livraria/pi3a/controller/BuscarUsuario.java
+++ Livraria/src/main/java/br/senac/tads/livraria/pi3a/controller/BuscarUsuario.java
@@ -6,72 +6,47 @@
 package br.senac.tads.livraria.pi3a.controller;
 
 import br.senac.tads.livraria.pi3a.dao.UsuarioDAO;
+import br.senac.tads.livraria.pi3a.model.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Fernanda
+ * @author Isaias
  */
+@WebServlet(name = "BuscarUsuario", urlPatterns = {"/buscarUsuario"})
 public class BuscarUsuario extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        UsuarioDAO dao = new UsuarioDAO();
-        
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        
+//        String papel = request.getParameter("papel");
+//        request.setAttribute("papelCliente", papel);
+        String b = request.getParameter("buscarNome");
+        
+        UsuarioDAO dao = new UsuarioDAO();
+        Usuario lista = dao.obterUsuario(b);
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        request.setAttribute("listaUsuario", lista);
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        RequestDispatcher dispatcher
+                = request.getRequestDispatcher("bootstrap/buscarUsuario.jsp");
+        dispatcher.forward(request, response);
+
+        try {
+            dispatcher.forward(request, response);
+        } catch (IOException ex) {
+
+        }
+
+    }
 
 }
