b657d989221032fc0557990600b0869fa4df783c
douglas.gsilva2 <douglas.gsilva2@CAS567313W10.senacsp.edu.br>
2017-06-08 21:37:03 -0300

    venda cruds

--- Livraria/src/main/java/br/senac/tads/livraria/pi3a/dao/JDBCProdutosDao.java
+++ Livraria/src/main/java/br/senac/tads/livraria/pi3a/dao/JDBCProdutosDao.java
@@ -81,25 +81,25 @@ public class JDBCProdutosDao extends ConexaoBD implements ProdutosDao {
 
     @Override
     public List<Produtos> listar() {
-        List<Produtos> produtos = new ArrayList<>();
         try {
+            List<Produtos> produtos = new ArrayList<>();
+            Produtos produto = new Produtos();
             String sql = "SELECT PRODNOME, PRODGENERO, PRODAUTOR, PRODVAL FROM PRODUTOS ORDER BY PRODNOME";
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            
+
             while (rs.next()) {
-                Produtos produto = new Produtos();              
+
                 produto.setProdNome(rs.getString("PRODNOME"));
                 produto.setProdGenero(rs.getString("PRODGENERO"));
                 produto.setProdAutor(rs.getString("PRODAUTOR"));
                 produto.setProdVal(rs.getDouble("PRODVAL"));
-                produtos.add(produto);
-
-                ps.close();
-                rs.close();
+                produtos.add(produto);    
             }
             return produtos;
+
         } catch (SQLException ex) {
+            
             Logger.getLogger(JDBCProdutosDao.class.getName()).log(Level.SEVERE, null, ex);
             throw new RuntimeException("Erro ao Listar Produtos em JDBCProdutosDao");
         }
@@ -127,9 +127,7 @@ public class JDBCProdutosDao extends ConexaoBD implements ProdutosDao {
             p.setProdVal(rs.getDouble("PRODVAL"));
             p.setProdDesc(rs.getString("PRODDESC"));
 
-            ps.close();
-            rs.close();
-
+            
             return p;
 
         } catch (SQLException ex) {
